975e76f590bb108116d9cc769a657c9ec93760b1
Matheus La Scala <matheus.lascala02@gmail.com>
2023-07-14 11:59:05 -0300

    Ajuste no model monstro, exibir as informações.

--- Models/Monstro.cs
+++ Models/Monstro.cs
@@ -1,7 +1,62 @@
-﻿namespace PokéGotchi.Models;
+﻿using System.Text.Json.Serialization;
+
+namespace PokéGotchi.Models;
+
+public class Habilidades
+{
+    [JsonPropertyName("ability")]
+    public Habilidade habilidade { get; set; }
+}
+
+public class Habilidade
+{
+    [JsonPropertyName("name")]
+    public string NomeHabilidade { get; set; }
+}
+
+public class Tipos
+{
+    [JsonPropertyName("type")]
+    public Tipo tipo { get; set; }
+}
+
+public class Tipo
+{
+    [JsonPropertyName("name")]
+    public string NomeTipo { get; set; }
+}
 
 internal class Monstro
 {
-    public int Nome { get; set; }
+    [JsonPropertyName("name")]
+    public string Nome { get; set; }
 
+    [JsonPropertyName("height")]
+    public int Altura { get; set; }
+
+    [JsonPropertyName("weight")]
+    public int Peso { get; set; }
+
+    [JsonPropertyName("abilities")]
+    public List<Habilidades> Habilidades { get; set; }
+
+    [JsonPropertyName("types")]
+    public List<Tipos> Tipos { get; set; }
+
+    public void ExibirMonstro()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Altura: {Altura}");
+        Console.WriteLine($"Peso: {Peso}");
+        Console.WriteLine($"Habilidades: ");
+        foreach (var hNome in Habilidades)
+        {
+            Console.WriteLine(hNome.habilidade.NomeHabilidade);
+        }
+        Console.WriteLine($"Tipo(s): ");
+        foreach (var tNome in Tipos)
+        {
+            Console.WriteLine(tNome.tipo.NomeTipo);
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,15 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using PokéGotchi.Models;
 using RestSharp;
+using System.Text.Json;
 
 Console.WriteLine("Start Here!");
 Main();
 
-// Eevee - Togepi - Ditto - Piplup - Fennekin
-// Minun - Plusle - Sylveon - Cinccino - zorua
-
 static void Main()
 {
+    ListaMonstros();
+}
+
+static void ListaMonstros()
+{
+    List<string> names = new List<string>
+    {
+        "eevee",
+        "togepi",
+        "ditto",
+        "piplup",
+        "fennekin",
+        "minun",
+        "plusle",
+        "sylveon",
+        "cinccino",
+        "zorua"
+    };
+
     List<Monstro> monstros = new List<Monstro>();
 
     /*
@@ -17,18 +34,30 @@ static void Main()
      * RestRequest -> Define a requisição -> GET
      */
 
-    var client = new RestClient($"https://pokeapi.co/api/v2/pokemon?offset=0&limte=1281"); //  https://pokeapi.co/api/v2/pokemon
-    RestRequest request = new RestRequest("", Method.Get);
-    var response = client.Execute(request);
+    foreach (string name in names)
+    {
+        var client = new RestClient($"https://pokeapi.co/api/v2/pokemon/{name}");
+        RestRequest request = new RestRequest("", Method.Get);
+        var response = client.Execute(request);
 
-    if (response.StatusCode == System.Net.HttpStatusCode.OK)
-    {
-        Console.WriteLine(response.Content);
+        if (response.StatusCode == System.Net.HttpStatusCode.OK && response.Content != null)
+        {
+            monstros.Add(JsonSerializer.Deserialize<Monstro>(response.Content)!); // ! operador indicando que não pode ser nullo   
+        }
+        else
+        {
+            // Verifica se ErrorMessage esta null e exibe a msg 
+            Console.WriteLine(response.ErrorMessage ?? "Erro não definido pela API!");
+        }
     }
-    else
+
+    int cont = 1;
+    foreach (Monstro monstro in monstros)
     {
-        // Verifica se ErrorMessage esta null e exibe a msg 
-        Console.WriteLine(response.ErrorMessage ?? "Erro não definido pela API!");
+        Console.WriteLine("---------------------------------------");
+        Console.WriteLine($"Criatura nº{cont}");
+        monstro.ExibirMonstro();
+        cont++;
     }
 
 }
